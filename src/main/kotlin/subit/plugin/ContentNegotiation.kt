d1fe0a60b914d117d7805bc82767031861fac3d1
nullaqua <nullaqua@outlook.com>
2024-09-13 22:57:42 +0800

    perf: 修改序列化/反序列化配置

--- src/main/kotlin/subit/plugin/ContentNegotiation.kt
+++ src/main/kotlin/subit/plugin/ContentNegotiation.kt
@@ -3,12 +3,14 @@ package subit.plugin
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import subit.debug
 
 /**
  * 安装反序列化/序列化服务(用于处理json)
  */
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.installContentNegotiation() = install(ContentNegotiation)
 {
     json(Json()
@@ -21,5 +23,13 @@ fun Application.installContentNegotiation() = install(ContentNegotiation)
         ignoreUnknownKeys = true
         // 宽松模式, 若字段类型不匹配, 则尝试转换
         isLenient = true
+        // 编码null
+        explicitNulls = true
+        // 允许特殊的浮点数值, 如NaN, Infinity
+        allowSpecialFloatingPointValues = true
+        // 忽略枚举大小写
+        decodeEnumsCaseInsensitive = true
+        // 允许尾随逗号
+        allowTrailingComma = true
     })
 }
