eba36c741a7a143f131dd672a452fa24a9934f91
nullaqua <nullaqua@outlook.com>
2024-10-05 01:51:37 +0800

    build: 更新依赖版本
    0. gradle升级到8.8
    1. exposed升级到0.55.0
    2. kotlin升级到2.0.20
    3. ktor升级到2.3.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ val swagger_ui_version: String by project
 val schema_kenerator_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.serialization") version "2.0.10"
-    id("io.ktor.plugin") version "2.3.11"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
+    id("io.ktor.plugin") version "2.3.12"
 }
 
 group = "subit"
