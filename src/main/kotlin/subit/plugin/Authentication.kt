eeb6ae12f551d29c4a3a0185ba01b4e2504a99dd
nullaqua <nullaqua@outlook.com>
2024-10-07 11:47:53 +0800

    perf: 添加登录验证的log

--- src/main/kotlin/subit/plugin/Authentication.kt
+++ src/main/kotlin/subit/plugin/Authentication.kt
@@ -10,6 +10,7 @@ import io.ktor.server.auth.Authentication
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import subit.config.apiDocsConfig
+import subit.logger.YouthWriteLogger
 import subit.router.utils.finishCall
 import subit.utils.HttpStatus
 import subit.utils.SSO
@@ -19,6 +20,7 @@ import subit.utils.SSO
  */
 fun Application.installAuthentication() = install(Authentication)
 {
+    val logger = YouthWriteLogger.getLogger()
     // 此登陆仅用于api文档的访问, 见ApiDocs插件
     basic("auth-api-docs")
     {
@@ -37,11 +39,12 @@ fun Application.installAuthentication() = install(Authentication)
             val token = it.request.header(HttpHeaders.Authorization) ?: run {
                 val t = parseHeaderValue(it.request.header(HttpHeaders.SecWebSocketProtocol))
                 val index = t.indexOfFirst { headerValue -> headerValue.value == "Bearer" }
-                if (index == -1) return@authHeader null
+                if (index == -1) return@run null
                 it.response.header(HttpHeaders.SecWebSocketProtocol, "Bearer")
                 t.getOrNull(index + 1)?.value?.let { token -> "Bearer $token" }
             }
-            token?.let(::parseAuthorizationHeader)
+            logger.config("request with token: $token")
+            runCatching { token?.let(::parseAuthorizationHeader) }.getOrNull()
         }
         authenticate {
             val token = it.token
